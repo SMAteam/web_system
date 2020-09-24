b6b4523e5f2edc7980b176c8b116999d89b22153
liuhuixiaoxin <1013294155@qq.com>
2020-09-24 09:37:15 +0800

    web后台框架

---
+++ earthquake/macroscopic/controller.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/23 17:03
+# @Author  : hui
+# @Email   : huihuil@bupt.edu.cn
+# @File    : controller.py
---
+++ earthquake/macroscopic/urls.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/23 17:04
+# @Author  : hui
+# @Email   : huihuil@bupt.edu.cn
+# @File    : urls.py
+
